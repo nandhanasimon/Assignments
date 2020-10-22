e2ad92be66f9e711d4ad8bb3c50885a4d2f80be3
nandhana simon <nandhanasimon4@gmail.com>
2020-10-22 00:30:27 -0400

    Assignment1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 0;
+            Console.WriteLine("Enter the String for Addition");
+            String Str = Console.ReadLine();
+           // Console.WriteLine("{0}", Str);
+            char[] seperator = { '+' };
+            string[] str1 = Str.Split(seperator);
+         
+            foreach (string s in str1)
+            {
+                //Console.WriteLine("the values we enetered are {0}\n ", s);
+                if (s.All(char.IsDigit))
+               value = value + int.Parse(s);
+            }
+            Console.WriteLine("The value is {0}", value);
+
+        }
+    }
+
+}
